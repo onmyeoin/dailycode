45a1b549a307d6b95c4547c3e1ef420ddffdad39
eoinor-da <eoinor@decisionanalytics.ai>
2024-12-12 09:58:35 +0000

    remove duplicates

---
+++ array/remove_duplicated_from_sorted_array.py
@@ -0,0 +1,12 @@
+class Solution:
+    def remove_duplicates(self, nums):
+        k = 0
+        seen = set()
+
+        for i in range(len(nums)):
+            if i not in seen:
+                seen.add(nums[i])
+                nums[k] = nums[i]
+                k += 1
+        
+        return k
