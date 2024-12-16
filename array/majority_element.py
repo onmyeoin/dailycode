2f79f68e0c7a6b5898875bfe3afe66b632752e5c
eoinor-da <eoinor@decisionanalytics.ai>
2024-12-16 12:03:14 +0000

    majority element

--- array/majority_element.py
+++ array/majority_element.py
@@ -1,7 +1,6 @@
 class Solution:
-    def majorityElement(self, nums):
-        from collections import Counter
-        output=Counter(nums).most_common()[0][0]
-        return output
-
-
+    def majorityElement(self, nums):    
+        nums = [2,2,1,1,1,2,2]
+        nums.sort()
+        n = len(nums)
+        nums[n//2]
