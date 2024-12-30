f7d9aca769175e00eabfdd44447624e2d3a22278
eoinor-da <eoinor@decisionanalytics.ai>
2024-12-30 16:38:11 +0000

    LL

---
+++ DSA/On2.py
@@ -0,0 +1,11 @@
+class Solution():
+    def print_items(self, n):
+        for i in range(n):
+            for j in range(n):
+                print(i,j)
+
+
+solution = Solution()
+
+solution.print_items(3)
+
