917f9c7ac2133aea8af174cf41253752313d2d40
eoinor-da <eoinoreilly2@gmail.com>
2025-01-03 12:40:30 +0000

    hash table

--- hashmap/testing.py
+++ hashmap/testing.py
@@ -1,26 +1,25 @@
-# hash map 
 class HashTable:
     def __init__(self, size = 7):
         self.data_map = [None] * size
 
-    def hash(self, key):
+    def __hash(self, key):
         my_hash = 0
         for letter in key:
-            my_hash = (my_hash + ord(letter) * 23) % len(self.data_map)
+            my_hash = (my_hash + ord(letter)*23) % len(self.data_map)
         return my_hash
     
     def print_table(self):
         for i, val in enumerate(self.data_map):
             print(i, ": ", val)
-    
+
     def set_item(self, key, value):
-        index = self.hash(key)
+        index = self.__hash(key)
         if self.data_map[index] == None:
             self.data_map[index] = []
         self.data_map[index].append([key, value])
-
-    def get_item(self,key):
-        index = self.hash(key)
+    
+    def get_item(self, key):
+        index = self.__hash(key)
         for i in range(len(self.data_map[index])):
             if self.data_map[index][i][0] == key:
                 return self.data_map[index][i][1]
@@ -33,14 +32,11 @@ class HashTable:
                 for j in range(len(self.data_map[i])):
                     all_keys.append(self.data_map[i][j][0])
         return all_keys
+                
 
 
 my_hash_table = HashTable()
+my_hash_table.set_item("Otis", 1)
+print(my_hash_table.get_item("Otis"))
 
-my_hash_table.set_item("screws", 100)
-my_hash_table.set_item("bolts", 200)
-my_hash_table.set_item("washers", 150)
-
-print(my_hash_table.keys())
-
-# my_hash_table.print_table()
+my_hash_table.print_table()
