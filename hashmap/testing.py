d260c55f96d60376d9d61d36a62bd9aa699912d5
eoinor-da <eoinoreilly2@gmail.com>
2025-01-08 20:44:11 +0000

    testing

--- hashmap/testing.py
+++ hashmap/testing.py
@@ -1,22 +1,21 @@
 class Solution:
     def lenLongestSubStr(self, s):
-        my_set = set()
-        maxLen = 0
         left = 0
+        maxLen = 0
+        my_set = set()
 
         for right in range(len(s)):
             while s[right] in my_set:
                 my_set.remove(s[left])
                 left += 1
             my_set.add(s[right])
-            maxLen = max(maxLen, right - left + 1)
-        
+            maxLen = max(maxLen, right - left +1)
         return maxLen
-    
+
 s = "bbbb"
 s2 = "abcabcbb"
-s3 = "pwwkew"
+s3 = "pwwkew3ka99"
 
 solution = Solution()
 
-print(solution.lenLongestSubStr(s2))
+print(solution.lenLongestSubStr(s3))
