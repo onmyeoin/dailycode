c30bbd3e106881ae08e9590d57111bc644533df3
eoinor-da <eoinoreilly2@gmail.com>
2025-01-13 16:57:30 +0000

    wip summary ranges

--- hashmap/testing.py
+++ hashmap/testing.py
@@ -1,15 +1,17 @@
 from collections import Counter
 import re
 
-def most_common_word(paragraph, banned):
-    clean_words = re.findall(r'\w+', paragraph.lower())
-    not_banned = [word for word in clean_words if word not in banned]
-    word_count = Counter(not_banned)
-    print(word_count.most_common(1)[0][0])
-
+class Solution:
+    def most_common_word(self, paragraph, banned):
+        clean_words = re.findall(r'\w+', paragraph.lower())
+        not_banned = [word for word in clean_words if word not in banned]
+        most_common = Counter(not_banned)
+        return(most_common.most_common(1)[0][0])
 
 
 paragraph = "Bob hit a ball, the hit BALL flew far after it was hit."
 banned = ["hit"]
 
-most_common_word(paragraph, banned)
+solution = Solution()
+
+print(solution.most_common_word(paragraph,banned))
